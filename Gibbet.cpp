72d47592ee30369c5ec1fbe3130095956cad1908
AnesioAndrade <nesin@outlook.com.br>
2022-05-06 20:29:25 -0300

    Definição do Construtor da Classe Gibbet

--- Gibbet.cpp
+++ Gibbet.cpp
@@ -1,1 +1,26 @@
 #include "Gibbet.h"
+
+Gibbet::Gibbet(std::string& word)
+{
+	lengthGibbetWord = word.length();
+	
+	//copia caracter por caracter de word para gibbetWord ;)
+	size_t count = 0;
+	for (char c : word) {
+		gibbetWord.push_back(word[count]);
+		count++;
+	}
+
+	//numero de tentativas será o tamanho da gibbetWord * 1.5;
+	factor = 1.5f;
+	numAttempts = static_cast <int>(lengthGibbetWord * factor);
+
+	AttemptsOver = false;
+	gameOver = false;
+	aLetter = 0;
+	
+}
+
+Gibbet::~Gibbet()
+{
+}
