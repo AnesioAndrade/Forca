fc6e9f1b5a8d3b0d8b210d19b12f764dec3137dd
AnesioAndrade <nesin@outlook.com.br>
2022-05-06 20:28:14 -0300

    Criação da classe Gibbet

--- Gibbet.h
+++ Gibbet.h
@@ -1,5 +1,23 @@
 #pragma once
+#include <iostream>
+#include <vector>
+#include <cstring>
 class Gibbet
 {
+public:
+	Gibbet(std::string& word);
+	~Gibbet();
+	
+private:
+	std::vector<char>gibbetWord{}; //palavra para enforcamento
+	std::vector<char>display{}; //painel 
+	std::vector<char>typedLetteres{}; //letras digitadas
+	char aLetter; //letra digitada
+	size_t lengthGibbetWord; //comprimento da palavra de enforcamento
+	int numAttempts; //número de tentativas
+	bool AttemptsOver; //tentátivas encerradas
+	bool gameOver; //fim de jogo
+
+	float factor; // fator para criar um numero de tentativas baseado em gibbetWord
 };
 
