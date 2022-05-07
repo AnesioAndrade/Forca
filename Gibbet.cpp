8828af1981335643c9543565f37567053b5441cc
AnesioAndrade <nesin@outlook.com.br>
2022-05-06 23:09:36 -0300

    Definido os metodos showDisplay e takeLetter

--- Gibbet.cpp
+++ Gibbet.cpp
@@ -2,30 +2,33 @@
 
 Gibbet::Gibbet(const std::string& word)
 {
-	lengthGibbetWord = word.length();
-	
+	lengthGibbetWord = word.length(); //obtem a quantidade de caracteres que a Palavra possuiu
+	rightLetters = 0; // zera a quantidade de letras certas
+	displayCount = 0;//zera o contador do Display
 	//copia caracter por caracter de word para gibbetWord ;)
 	size_t count = 0;
 	for (char c : word) {
 		gibbetWord.push_back(word[count]);
 		count++;
 	}
-
 	//numero de tentativas será o tamanho da gibbetWord * 1.5;
 	factor = 1.5f;
 	numAttempts = static_cast <int>(lengthGibbetWord * factor);
-
 	AttemptsOver = false;
 	gameOver = false;
 	aLetter = 0;
-	
 	//Apaga e preenche o Diplay com '_' conforme o tamanho de gibbetWord
-	resetDisplay();
-		
+	resetDisplay();		
 }
 
 Gibbet::~Gibbet()
 {
+}
+
+void Gibbet::takeLetter(const char& c)
+{
+	aLetter = c;
+	showDisplay();
 }
 
 void Gibbet::resetDisplay()
@@ -34,7 +37,6 @@ void Gibbet::resetDisplay()
 	if (!display.empty()) {
 		display.clear();
 	}
-	
 	for (size_t c = 0; c < lengthGibbetWord; c++) {
 		display.push_back('_');
 	}
@@ -42,9 +44,17 @@ void Gibbet::resetDisplay()
 
 void Gibbet::showDisplay()
 {
-	for (char c : display) {
-		std::cout << c << std::endl;
+	for (char l : gibbetWord) {
+		if (l == aLetter) {
+			display.at(displayCount) = aLetter;
+			rightLetters++;
+		}
+		displayCount++;
 	}
+	for (char d : display) {
+		std::cout << d << " ";
+	}
+	displayCount = 0;
 }
 
 
--- Gibbet.h
+++ Gibbet.h
@@ -7,19 +7,21 @@ class Gibbet
 public:
 	Gibbet(const std::string& word);
 	~Gibbet();
+	void takeLetter(const char& c);
+
 	
 private:
 	std::vector<char>gibbetWord{}; //palavra para enforcamento
 	std::vector<char>display{}; //painel 
 	std::vector<char>typedLetteres{}; //letras digitadas
 	char aLetter; //letra digitada
+	size_t displayCount;//contador do display
 	size_t lengthGibbetWord; //comprimento da palavra de enforcamento
+	size_t rightLetters; //para cada letra que estiver certa, adiciona +1
 	int numAttempts; //número de tentativas
 	bool AttemptsOver; //tentátivas encerradas
 	bool gameOver; //fim de jogo
-
 	float factor; // fator para criar um numero de tentativas baseado em gibbetWord
-
 	void resetDisplay();
 	void showDisplay();
 };
